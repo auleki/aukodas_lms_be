47823af18f36b338c0640b799daea101caffe09b
auleki <kodagiwa@gmail.com>
2022-12-12 08:31:38 +0100

    define controller for auth routes

---
+++ controlllers/auth.controller.js
@@ -0,0 +1,24 @@
+
+export const register = async (req, res) => {
+    try {
+        res.status(201).json({ message: "User is successfully created" })
+    } catch (error) {
+        res.status(400).json({ message: "There is a problem here" })
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        res.status(201).json({ message: "Logged in successfully" })
+    } catch (error) {
+        res.status(400).json({ message: "There is a problem here" })
+    }
+}
+
+export const resetPassword = async (req, res) => {
+    try {
+        res.status(201).json({ message: "Password has been reset successfully" })
+    } catch (error) {
+        res.status(400).json({ message: "There is a problem here" })
+    }
+}
