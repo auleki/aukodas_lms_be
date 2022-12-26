7d3c7377848117c0defe70e7fecd841697a31f89
auleki <kodagiwa@gmail.com>
2022-12-26 11:10:17 +0100

    include method to authenticate JWT tokens

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -10,4 +10,22 @@ export const jwtToken = (data) => {
 
 export const verifyToken = (token) => {
     return jwt.verify(token, JWT_SECRET)
-} 
+}
+
+export const authenticateJwt = (req, res, next) => {
+    // console.log('req from mid', req.headers)
+    const authHeader = req.headers.authorization
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1]
+        jwt.verify(token, JWT_SECRET, (err, user) => {
+            if (err) {
+                return res.send(403).json({ error: err, message: "Doing things like this would get you banned" })
+            }
+            req.user = user
+            next()
+        })
+    } else {
+        res.status(401).json({ error: "You are not authorized" })
+    }
+}
